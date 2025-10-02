faaa15303702bcbdd3cae1942fabe180629e3ede
Jollyjoo <156919086+Jollyjoo@users.noreply.github.com>
2025-10-02 16:54:31 +0300

    Delete db.php

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-// db.php - MySQL connection settings for ennakointi platform
-$dsn = 'mysql:host=tulevaisuusluotain.fi;dbname=catbxjbt_ennakointi;charset=utf8mb4';
-$db_user = 'catbxjbt_Christian';
-$db_pass = 'Juustonaksu5';
-// Usage: $pdo = new PDO($dsn, $db_user, $db_pass, [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"]);
-?>
