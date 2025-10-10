14f48676c0963451256eb14aa210596ea8aad0af
Jollyjoo <156919086+Jollyjoo@users.noreply.github.com>
2025-10-10 12:59:39 +0300

    Update fetchTyottomat_12r5.php

--- fetchTyottomat_12r5.php
+++ fetchTyottomat_12r5.php
@@ -63,7 +63,7 @@ function logMessage($message) {
 }
 
 // Funktio, joka generoi viimeisimmät N kuukautta (mukaan lukien viimeisin kuukausi)
-function getRecentKuukaudet($n = 5) {
+function getRecentKuukaudet($n = 50) {
     $months = [];
     $base = strtotime(date('Y-m-01')); // Ensimmäinen päivä kuluvasta kuukaudesta
     for ($i = 1; $i <= $n; $i++) {
@@ -81,7 +81,7 @@ function main() {
         $jsonArray = json_decode(file_get_contents($jsonFile), true);
 
         // Päivitetään Kuukausi-arvot viimeisimmille 5 kuukaudelle
-        $recentKuukaudet = getRecentKuukaudet(5);
+        $recentKuukaudet = getRecentKuukaudet(50);
         foreach ($jsonArray['query'] as &$query) {
             if ($query['code'] === 'Kuukausi') {
                 $query['selection']['values'] = $recentKuukaudet;
